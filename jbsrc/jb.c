1083c2dad940dd0c535640f65ad2141d44b80582
Erik van Pienbroek <epienbro@fedoraproject.org>
2012-08-19 17:51:01 +0200

    Link explicitly against X11
    Fixes FTBFS on modern gcc/binutils environments
    See http://fedoraproject.org/wiki/UnderstandingDSOLinkChange
    for more details about this subject. Patch by Erik van Pienbroek

--- jbsrc/jb.c
+++ jbsrc/jb.c
@@ -447,6 +447,7 @@ jb_package_add_resources (void)
   jb_compile_options_add_ldflags(object->compile_options, "-Wl,--export-dynamic");
 
   jb_compile_options_add_libs(object->compile_options, "-lm");
+  jb_compile_options_add_libs(object->compile_options, "-lX11");
 
   jb_compile_options_add_package(object->compile_options, "gettext");
   jb_compile_options_add_package(object->compile_options, "gnome");
